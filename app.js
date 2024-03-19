78cd4b1cf78bac8ec96aefbcbc53ee3f60962688
puneet-develops <puneet.k@antino.com>
2024-03-19 18:21:04 +0530

    connection with atlas

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
 const express =require("express");
+const { MongoClient } = require('mongodb');
 const app=express();
 const mongoose =require("mongoose");
-const MONGO_URL='mongodb://127.0.0.1:27017/wanderlust';
+const MONGO_URL='mongodb+srv://nikkuemwe:Rkrajput@123@cluster0.lz3bm7p.mongodb.net/wanderlust?retryWrites=true&w=majority&appName=Cluster0';
 const Listing=require("./models/listing");
 const path = require("path");
 const methodOverride=require("method-override"); 
 const engine = require("ejs-mate");
-main().then(()=>{
-    console.log("connected to db");
-}).catch((err)=>{
-    console.log(err);
-});
+
+
+
+// async function main(){
+//     await mongoose.connect(MONGO_URL)
+// };
+
+
 
 async function main(){
-    await mongoose.connect(MONGO_URL)
+    const uri = "mongodb+srv://nikkuemwe:Rkrajput%40123@cluster0.lz3bm7p.mongodb.net/wanderlust?retryWrites=true&w=majority&appName=Cluster0";
+   // const client = new MongoClient(uri)
+    try {
+        await mongoose.connect(uri)
+        console.log("connected to db");
+
+        // Make the appropriate DB calls
+        //await  listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        //await client.close();
+        console.log("here is finally block")
+    }
+}
+main()
+
+
+// main().catch(console.error);
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
 
 app.set("view engine","ejs");
