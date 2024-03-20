51c75709bbec682503e4ab54cb98a772316b8aab
puneet-develops <puneet.k@antino.com>
2024-03-20 18:01:59 +0530

    halfphasedone

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose =require("mongoose");
 const MONGO_URL='mongodb+srv://nikkuemwe:Rkrajput@123@cluster0.lz3bm7p.mongodb.net/wanderlust?retryWrites=true&w=majority&appName=Cluster0';
 const Listing=require("./models/listing");
 const path = require("path");
-const methodOverride=require("method-override"); 
+const methodOverride=require("method-override");  
 const engine = require("ejs-mate");
 
 
@@ -14,9 +14,8 @@ const engine = require("ejs-mate");
 //     await mongoose.connect(MONGO_URL)
 // };
 
-
-
 async function main(){
+
     const uri = "mongodb+srv://nikkuemwe:Rkrajput%40123@cluster0.lz3bm7p.mongodb.net/wanderlust?retryWrites=true&w=majority&appName=Cluster0";
    // const client = new MongoClient(uri)
     try {
@@ -33,7 +32,7 @@ async function main(){
         console.log("here is finally block")
     }
 }
-main()
+main();
 
 
 // main().catch(console.error);
@@ -56,18 +55,6 @@ app.get("/",(req,res)=>{
     res.send("hi this is root page");   
 });
 
-// app.get("/testListing",async(req,res)=>{
-//     let samplelisting =new Listing({
-//         title:"my new villa",
-//         description:"by the beach best hotel",
-//         price:20000,
-//         location:"calangute,Goa",
-//         country:"India",
-//     });
-//     await samplelisting.save();
-//     console.log("sample was saved");
-//     res.send("successful testing");
-// });
 // all listings (INDEX ROUTE)
 app.get("/listings",async(req,res)=>{
     const allListing=await Listing.find();
@@ -77,7 +64,7 @@ app.get("/listings",async(req,res)=>{
 //new route 
 app.get("/listings/new",(req,res)=>{
     res.render("../views/listings/new.ejs");
-})
+});
 app.get("/listings/:id",async(req,res)=>{
     let {id} = req.params;
     console.log(id)
@@ -97,7 +84,7 @@ app.get("/listings/:id/edit",async(req,res)=>{
     const listing= await Listing.findById(id);
     console.log(listing);
     res.render("../views/listings/edit.ejs",{listing});
-})
+});
 // update route put request 
 app.put("/listings/:id",async(req,res)=>{
     let{id}=req.params;
