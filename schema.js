6444a7adcaf3a9cd5d18f9b0ad6615c58b477bd8
puneet-develops <puneet.k@antino.com>
2024-04-01 17:10:35 +0530

    release-1.2.1 completed all Review related ISSUES resolved

--- schema.js
+++ schema.js
@@ -9,6 +9,12 @@ module.exports.listingSchema=Joi.object({
         image: Joi.object({
             filename: Joi.string().default("listingimage"),
             url: Joi.string().allow("",null)
-    })
+        })
 }).required()
+});
+module.exports.reviewSchema=Joi.object({
+    review: Joi.object({
+    rating:Joi.number().required().min(1).max(5),
+    comment:Joi.string().required(),
+    }).required()
 });
