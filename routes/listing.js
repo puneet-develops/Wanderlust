115fddf214b99413b288d7d4297da58f6c7a7ce5
puneet-develops <puneet.k@antino.com>
2024-04-15 18:24:57 +0530

    release-1.3.1

--- routes/listing.js
+++ routes/listing.js
@@ -6,40 +6,41 @@ const { listingSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
-
-// all listings (INDEX ROUTE)
-router.get("/", wrapAsync(listingController.index));
-//new route
-router.get("/new", isLoggedIn, listingController.renderNewForm);
-//show route
-router.get("/:id", wrapAsync(listingController.showListings));
-// create route
-router.post(
-  "/",
-  isLoggedIn,
-  validateListing,
-  wrapAsync(listingController.createListings)
-);
-//edit route
+const multer=require('multer');
+const {storage}=require("../cloudConfig.js");
+const upload = multer({storage});
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  .post(
+    isLoggedIn,
+    
+    upload.single('listing[image][url]'),
+    validateListing,
+    wrapAsync(listingController.createListings)
+  );
+  
+  router.get("/new", isLoggedIn, listingController.renderNewForm);
+router
+  .route("/:id")
+  .get( wrapAsync(listingController.showListings))
+  .put(
+    
+    isLoggedIn,
+    isOwner,
+    validateListing,
+    wrapAsync(listingController.updateListings)
+  )
+  .delete(
+    isLoggedIn,
+    isOwner,
+    wrapAsync(listingController.deleteListings)
+  );
 router.get(
   "/:id/edit",
   isLoggedIn,
   isOwner,
   wrapAsync(listingController.renderEditForm)
 );
-// update route put request
-router.put(
-  "/:id",
-  isLoggedIn,
-  isOwner,
-  validateListing,
-  wrapAsync(listingController.updateListings)
-);
-//delete route
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isOwner,
-  wrapAsync(listingController.deleteListings)
-);
+
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -4,51 +4,24 @@ const router = express.Router();
 const User = require("../models/user.js");
 const { route } = require("./listing");
 const passport = require("passport");
-const {saveredirectUrl}=require("../middleware.js");
+const { saveredirectUrl } = require("../middleware.js");
+const userController = require("../controllers/users.js");
 
-router.get("/signup", (req, res) => {
-  res.render("users/signup.ejs");
-});
+router
+  .route("/signup")
+  .get(userController.renderSignupForm)
+  .post(wrapAsync(userController.signup));
+router
+  .route("/login")
+  .get(userController.renderLoginForm)
+  .post(
+    saveredirectUrl,
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true,
+    }),
+    userController.login
+  );
 
-router.post(
-  "/signup",
-  wrapAsync(async (req, res) => {
-    try {
-      let { username, email, password } = req.body;
-      const newUser = new User({ email, username });
-      const registeredUser = await User.register(newUser, password);
-      console.log(registeredUser);
-      req.login(registeredUser,(err)=>{
-        if(err){
-          return next(err);
-        }
-        req.flash("success", "Welcome to WanderLust!");
-        res.redirect("/listings");
-      })
-     
-    } catch (e) {
-      req.flash("error", e.message);
-      res.redirect("/signup");
-    }
-  })
-);
-
-router.get("/login",(req,res)=>{
-    res.render("users/login.ejs");
-});
-router.post("/login",saveredirectUrl,passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),async(req,res)=>{
-    req.flash("success","welcome back to wanderLust ");
-    let redirectUrl=res.locals.redirectUrl||"/listings";
-    res.redirect(redirectUrl);
-});
-
-router.get("/logout",(req,res,next)=>{
-  req.logout((err)=>{
-    if(err){
-      return next(err);
-    }
-    req.flash("success","You are logged out !");
-    res.redirect("/listings");
-  })
-})
+router.get("/logout", userController.logout);
 module.exports = router;
